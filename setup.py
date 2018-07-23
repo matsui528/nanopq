bc21a76d6a3608ce9387f2a63c8e6191b6000b47
Yusuke Matsui <matsui@nii.ac.jp>
2018-07-24 02:26:27 +0900

    version 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
     name='nanopq',
-    version='0.1.0',
+    version='0.1.1',
     description='Product quantization for nearest neighbor search in a single python file',
     long_description=readme,
     long_description_content_type='text/markdown',
