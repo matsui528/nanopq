eb9763a47f4ae16a19b14b2050e29c3422038fc6
Yusuke Matsui <matsui@nii.ac.jp>
2018-07-25 11:54:57 +0900

    simple license for setup.py. start to 0.1.4dev1

--- setup.py
+++ setup.py
@@ -3,20 +3,16 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='nanopq',
-    version='0.1.3',
+    version='0.1.4.dev1',
     description='Product quantization for nearest neighbor search in a single python file',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Yusuke Matsui',
     author_email='matsui528@gmail.com',
     url='https://github.com/matsui528/nanopq',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=['numpy', 'scipy'],
 )
-
