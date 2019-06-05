addadce7392f6b9c1cb12a93b247328e653124eb
Yusuke Matsui <matsui528@gmail.com>
2019-06-05 18:33:14 +0900

    v0.1.7

--- nanopq/__init__.py
+++ nanopq/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['PQ', 'OPQ', 'DistanceTable', 'nanopq_to_faiss', 'faiss_to_nanopq']
-__version__ = '0.1.7.dev1'
+__version__ = '0.1.7'
 
 from .pq import PQ, DistanceTable
 from .opq import OPQ
